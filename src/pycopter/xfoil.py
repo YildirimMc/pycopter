cc6db0f8a8464ece9549be950e767d83d1a8f9f9
Mehar Can Yildirim <yildirimmehar@gmail.com>
2024-07-08 20:18:02 +0200

    Removed function get_polar. Function read_polar now uses numpy. Ability to simulate polars for multiple alfas is added. Added  other QOL changes.

--- src/pycopter/xfoil.py
+++ src/pycopter/xfoil.py
@@ -1,40 +1,46 @@
 import subprocess
+import os
+import numpy as np
 
 class Xfoil():
-    def __init__(self, airfoil, mach, reynolds):
+    """Contains methods to communicate with the XFOIL.exe instance running in the background."""
+
+    def __init__(self, airfoil, new_polar=True):
         self.exe_path = "data/XFOIL6.99/xfoil.exe"
         self.output_path = "data/XFOIL6.99/polar.txt"
         self.airfoil = airfoil
-        self.mach = mach
-        self.reynolds = reynolds
-        self.polar = []
+        self.max_theta = 15
         
         self.process = subprocess.Popen(self.exe_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
 
-    def read_polar(self, path):
-        with open(path, "r") as txt:
-            data = txt.readlines()
-            data = data[-1].split("   ")
-            return float(data[2]), float(data[3]), float(data[4]), float(data[5]), float(data[6]), float(data[7].strip())
-
-    def simulate(self, alfa):
-        inputs = [self.airfoil, "oper", "iter 400", "v", str(self.reynolds), f"mach {self.mach}", "pacc", self.output_path + "\n", f"alfa {alfa}"]
-
+        if new_polar and os.path.exists(self.output_path):
+            os.remove(self.output_path) 
+        
+    def simulate(self, mach, reynolds):
+        """
+        """
+        inputs_init = [self.airfoil, "oper", "iter 400", "v", str(reynolds), f"mach {mach}", "pacc", self.output_path + "\n"]
+        inputs = [f"alfa {alfa}" for alfa in np.arange(0, self.max_theta + 1)]
         command = ""
+        for input in inputs_init:
+            command = command + input + "\n"
         for input in inputs:
             command = command + input + "\n"
         
         output, error = self.process.communicate(input=command)
-        self.polar = self.readPolar(self.output_path)
-        return self.polar
-    
-    def get_polar(self):
-        return self.polar
+        
+        # This just gives error all the time.
+        # if output.find("Convergence failed") != -1:
+        #     print("XFOIL convergence has failed due to highly turbulent flow. Polars for high theta are expected to be faulty. Confirm the alfa vs cl vs cd plots.")
+
+    def read_polar(self):
+        """
+        """
+        return np.genfromtxt(self.output_path, skip_header=12)
+
 
 if __name__ == "__main__":
-    xfoil = Xfoil("naca0012", 0.6, 200000)
-    polar_info = xfoil.simulate(alfa=6)
-    # polar_info = xfoil.get_polar()
-    print(polar_info[0])
+    xfoil = Xfoil("naca23012", True)
+    xfoil.simulate(0.3, 4000000)
 
 
